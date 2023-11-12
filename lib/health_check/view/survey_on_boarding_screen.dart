0679c82ee36ba8cd2d0da2a1d12b13e77203f3dc
yeongwoo <jyy0223@naver.com>
2023-11-12 23:23:18 +0900

    [add] 설문조사 시작 페이지 구현 완료

--- lib/health_check/view/survey_on_boarding_screen.dart
+++ lib/health_check/view/survey_on_boarding_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:health_solution/common/layout/default_button.dart';
 
+import '../../common/const/text_style.dart';
 import '../../common/layout/default_appbar.dart';
 import '../../common/layout/default_layout.dart';
 
@@ -10,8 +12,32 @@ class SurveyOnBoardingScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultLayout(
       appbar: DefaultAppBar(title: "내 건강 체크"),
-      child: Center(
-        child: Text("내 건강 체크"),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 32.0, horizontal: 24.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(height: 8.0),
+            const Text(
+              '내 건강 상태를 체크하기 위해\n설문조사를 진행해 주세요',
+              style: MyTextStyle.headTitle,
+            ),
+            const SizedBox(height: 40.0),
+            SizedBox(
+              child: Image.asset(
+                'asset/img/heart.png',
+                fit: BoxFit.contain,
+                width: MediaQuery.of(context).size.width * 2 / 3,
+                height: MediaQuery.of(context).size.width * 2 / 3,
+              ),
+            ),
+            const SizedBox(height: 40.0),
+            DefaultElevatedButton(
+              onPressed: () {},
+              child: const Text('설문조사 시작하기'),
+            ),
+          ],
+        ),
       ),
     );
   }
