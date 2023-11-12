4e08e17ba830a5102e38e77b03acba07dbbfd07e
yeongwoo <jyy0223@naver.com>
2023-11-12 18:33:53 +0900

    [add] text style에 자간, 높이 조절 추가

--- lib/common/const/text_style.dart
+++ lib/common/const/text_style.dart
@@ -4,45 +4,63 @@ class MyTextStyle {
   static const appName = TextStyle(
     fontSize: 36.0,
     fontWeight: FontWeight.bold,
+    letterSpacing: -1.025,
+    height: 1.2,
   );
 
   static const headTitle = TextStyle(
     fontSize: 24.0,
     fontWeight: FontWeight.bold,
+    letterSpacing: -1.025,
+    height: 1.2,
   );
 
   static const bodyTitleBold = TextStyle(
     fontSize: 20.0,
     fontWeight: FontWeight.bold,
+    letterSpacing: -1.025,
+    height: 1.2,
   );
 
   static const bodyBold = TextStyle(
     fontSize: 18.0,
     fontWeight: FontWeight.bold,
+    letterSpacing: -1.025,
+    height: 1.2,
   );
 
   static const bodyMedium = TextStyle(
     fontSize: 18.0,
     fontWeight: FontWeight.w500,
+    letterSpacing: -1.025,
+    height: 1.2,
   );
 
   static const bodyRegular = TextStyle(
     fontSize: 18.0,
     fontWeight: FontWeight.w300,
+    letterSpacing: -1.025,
+    height: 1.2,
   );
 
   static const descriptionBold = TextStyle(
     fontSize: 16.0,
     fontWeight: FontWeight.bold,
+    letterSpacing: -1.025,
+    height: 1.2,
   );
 
   static const descriptionMedium = TextStyle(
     fontSize: 16.0,
     fontWeight: FontWeight.w500,
+    letterSpacing: -1.025,
+    height: 1.2,
   );
 
   static const descriptionRegular = TextStyle(
     fontSize: 16.0,
     fontWeight: FontWeight.w400,
+    letterSpacing: -1.025,
+    height: 1.2,
   );
 }
